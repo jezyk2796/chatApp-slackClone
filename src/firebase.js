b4ffc830150671fbd5a17f29135eb69deb8b0fd8
Szymon <jezyk2796@op.pl>
2021-02-25 23:54:30 +0100

    code cleanup and organisation. Added firebase file and react router package

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA4Hf0C_2Jpxac4J4GhrpxSX6hi01hnvaw",
+    authDomain: "chatapp-slackclone.firebaseapp.com",
+    projectId: "chatapp-slackclone",
+    storageBucket: "chatapp-slackclone.appspot.com",
+    messagingSenderId: "582575979644",
+    appId: "1:582575979644:web:02d41f68efe6d61e3bddf9"
+};
